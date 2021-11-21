ba272f110539420fe9301839effbc346e7110667
willzed26 <wiliamwijaya101@gmail.com>
2021-11-22 03:12:51 +0700

    repair heroku

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ app.use('/', indexRouter);
 // 	response.end();
 // });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, () => {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
